145ae1f861f8db58861fe94ac5fe66dc9b2034b1
Pedro Otávio <p.o.maxado@gmail.com>
2017-05-21 10:46:33 -0300

    ignored some files

--- tests/calc_time.sh
+++ tests/calc_time.sh
@@ -3,7 +3,7 @@
 for file in $(seq 1000 1000 10000)
 do
     echo "test_$file" >> big_tests_log.txt
-    for i in $(seq 1 30)
+    for i in $(seq 1 10)
     do
         elapsed=$(/usr/bin/time -f "%e" -a -o "big_tests_log.txt" ../tp1 < big_tests/test_$file > /dev/null)
     done
